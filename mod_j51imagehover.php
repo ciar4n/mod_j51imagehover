1978418ddb9a1d9eac5c8504573b760ce1a0df1d
ciar4n <ciaran@joomla51.com>
2017-01-18 08:25:42 +0000

    Default value for params

--- mod_j51imagehover.php
+++ mod_j51imagehover.php
@@ -17,29 +17,25 @@ $helper = new modJ51ImageHover();
 
 // Define variables
 $imagehover_images    = $params->get('imagehover_images');
-$j51_imghvr           = $params->get('j51_imghvr');
-$j51_num_images       = $params->get('j51_num_images'); 
-$j51_randomize        = $params->get('j51_randomize');
-$j51_image_width      = $params->get('j51_image_width');
-$j51_image_width_tabl = $params->get('j51_image_width_tabl');
-$j51_image_width_tabp = $params->get('j51_image_width_tabp');
-$j51_image_width_mobl = $params->get('j51_image_width_mobl');
-$j51_image_width_mobp = $params->get('j51_image_width_mobp');
-$j51_image_margin_x   = $params->get('j51_image_margin_x');
-$j51_image_margin_y   = $params->get('j51_image_margin_y');
-$j51_image_padding_x  = $params->get('j51_image_padding_x');
-$j51_image_padding_y  = $params->get('j51_image_padding_y');
-$j51_overlay_color    = $params->get('j51_overlay_color');
-$j51_title_color      = $params->get('j51_title_color');
-$j51_text_color       = $params->get('j51_text_color');
-$j51_text_align       = $params->get('j51_text_align');
-$j51_text_vert_align  = $params->get('j51_text_vert_align');
-$j51_bg_color         = $params->get('j51_bg_color');
-$j51_border_color     = $params->get('j51_border_color');
-$j51_animate_class    = $params->get('j51_animate_class');
+$j51_imghvr           = $params->get('j51_imghvr', 'imghvr-fade-in-up');
+$j51_image_width      = $params->get('j51_image_width', '25%');
+$j51_image_width_tabl = $params->get('j51_image_width_tabl', '33.333%');
+$j51_image_width_tabp = $params->get('j51_image_width_tabp', '33.333%');
+$j51_image_width_mobl = $params->get('j51_image_width_mobl', '50%');
+$j51_image_width_mobp = $params->get('j51_image_width_mobp', '100%');
+$j51_image_margin_x   = $params->get('j51_image_margin_x', '10');
+$j51_image_margin_y   = $params->get('j51_image_margin_y', '10');
+$j51_image_padding_x  = $params->get('j51_image_padding_x', '20');
+$j51_image_padding_y  = $params->get('j51_image_padding_y', '20');
+$j51_overlay_color    = $params->get('j51_overlay_color', '#135796');
+$j51_title_color      = $params->get('j51_title_color', '#ffffff');
+$j51_text_color       = $params->get('j51_text_color', '#ffffff');
+$j51_text_align       = $params->get('j51_text_align', 'center');
+$j51_text_vert_align  = $params->get('j51_text_vert_align', 'center');
 $j51_moduleid         = $module->id;
 $j51_title            = $params->get('j51_title');
 $j51_image            = $params->get('j51_image');
+$j51_image_alt         = $params->get('j51_image_alt')
 $j51_target_url       = $params->get('j51_target_url');
 
 require(JModuleHelper::getLayoutPath('mod_j51imagehover'));
